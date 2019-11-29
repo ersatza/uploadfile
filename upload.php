8c53cae49d5cb86a124d03f0aafaa44830d449b3
ersatza <58320716+ersatza@users.noreply.github.com>
2019-11-29 06:29:50 +0100

    Add files via upload

---
+++ upload.php
@@ -0,0 +1,31 @@
+<?php
+// bdlo directory
+$target_dir = "";
+$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
+$uploadOk = 1;
+$FileType = strtolower(pathinfo($target_file,PATHINFO_EXTENSION));
+
+if (file_exists($target_file)) {
+    echo " already exists ";
+    $uploadOk = 0;
+}
+if ($_FILES["fileToUpload"]["size"] > 500000) {
+    echo "Sorry, your file is too large.";
+    $uploadOk = 0;
+}
+if($FileType != "py") {
+    echo " File format = .py ";
+    $uploadOk = 0;
+}
+
+if ($uploadOk == 0) {
+    echo " Sorry, your file was not uploaded. ";
+
+} else {
+    if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+        echo "The file ". basename( $_FILES["fileToUpload"]["name"]). " has been uploaded.";
+    } else {
+        echo "Sorry, there was an error uploading your file.";
+    }
+}
+?>
